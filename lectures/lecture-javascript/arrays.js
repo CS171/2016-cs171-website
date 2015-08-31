677d110fc934f6a4b2eea95ed6354808f0c3ecdb
Alexander Lex <alex@sci.utah.edu>
2015-08-30 19:54:23 -0600

    worked on JS lecture

---
+++ lectures/lecture-javascript/arrays.js
@@ -0,0 +1,3 @@
+var c = [0,1,2];
+var e = []; // empty array declaration
+console.log(c[0]);
--- lectures/lecture-javascript/variables.js
+++ lectures/lecture-javascript/variables.js
@@ -1,5 +1,15 @@
+// -- global variables --
+// an integer
 a = 0;
+// a string
 b = "1";
+// an array
 c = [1, 2, "3", [4]];
+// a boolean
 f = false;
+// redefining f as a float
 f = 34.56;
+
+// -- "local" variables --
+var name="Alex";
+var age=34;
